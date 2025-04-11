7dcf732aa68fb48058afe15bb37e67387d05f4dd
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-04-11 13:12:25 -0400

    ADD: Montserrat Font

--- landing.php
+++ landing.php
@@ -30,6 +30,11 @@ $listings = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="assets/css/landing.css">
 </head>
 <body>
